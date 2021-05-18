a8689671903d69d03062ea6aa2f47033bd1cca46
Anouar Tobji <anouar.24071983@gmail.com>
2021-05-18 23:52:54 +0100

    props

--- src/App.js
+++ src/App.js
@@ -6,14 +6,15 @@ import image from './John-Doe.jpg'
 
 function App() {
   function btn(name){
-   alert(`my name is : `)
+   alert(`my name is : ${name} `)
 }
 
   return (
     <>
-       <Profile name="anouar" bio="bio1" profession="dev1" data={btn}><img src={image }style={{borderRadius: '50%'}}/></Profile>
-       <Profile name="anouar" bio="bio2" profession="dev2" ><img src={image}style={{borderRadius: '50%'}}/></Profile>
-       <Profile name="anouar" bio="bio3" profession="dev3" ><img src={image}style={{borderRadius: '50%'}}/></Profile>
+       <Profile name="anouar1" bio="bio1" profession="dev1" data={btn}><img src={image }style={{borderRadius: '50%'}}/></Profile>
+       <Profile name="anouar2" bio="bio2" profession="dev2"data={btn} ><img src={image}style={{borderRadius: '50%'}}/></Profile>
+       <Profile name="anouar3" bio="bio3" profession="dev3" data={btn}><img src={image}style={{borderRadius: '50%'}}/></Profile>
+       <Profile bio="bio4"  data={btn}></Profile>
     </>
   );
 }
--- src/profile/Profile.js
+++ src/profile/Profile.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import PropTypes from "prop-types";
+import PropTypes  from "prop-types";
 // import '../App.css'
 
-const Profile=({name,children,bio,profession})=>{
+const Profile=({name,children,bio,profession ,data})=>{
     
-    function btn(){
-    return alert('my name is :' +{name})
-}
+//     function btn(){
+//     return alert('my name is :' +{name})
+// }
 return (
 <div style={ { backgroundColor: 'white',
   width: '400px' ,
@@ -15,14 +15,27 @@ padding: '50px',
 border: '1px solid rgba(0, 0, 0, 0.205)'
 
 }}>
+  {/* {data(name)} */}
     <h1>Fullname :{name}</h1>
      <p>{children}</p>
     
     <h1>Bio :{bio}</h1>
     <h1>Profession :{profession}</h1>
-    <button onClick={()=>{btn()}}>ClickMe </button>
-   
+    {/* <button onClick={()=>{btn()}}>ClickMe </button> */}
+    <button onClick={()=>data(name)}>ClickMe </button>
     </div>
 )
+
+Profile.prototype = {
+  name:PropTypes.String,
+  bio :PropTypes.string.isRequired,
+  profession:PropTypes.string,
+  data:PropTypes.func,
+  children:PropTypes.object
+}
+}
+Profile.defaultProps ={
+name:"no",
+profession:"no profession"
 }
 export default Profile
